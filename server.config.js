868a6860609b8d2f8c966ba11fa0c9d208e0cf40
antimo2001 <antimo2001@gmail.com>
2018-07-14 18:53:14 -0400

    update server.config

--- server.config.js
+++ server.config.js
@@ -3,7 +3,8 @@ const testdataDir = path.join(__dirname, 'testdata');
 
 module.exports = {
   port: process.env.PORT || 4242,
-  storageDir: process.env.VIDEO_STORAGE_DIR || testdataDir,
-  rxFileFilter: (/\.(mp4|MTS)/i),
-  rxFolders: (/\b(3|4|5|6|7)\b/i),
+  // storageDir: process.env.VIDEO_STORAGE_DIR || testdataDir,
+  storageDir: testdataDir,
+  rxFileFilter: (/\.(mp4|MTS|mpg)/i),
+  rxFolders: (/\b[3-9]|(B[LR]\d*)\b/i),
 }
