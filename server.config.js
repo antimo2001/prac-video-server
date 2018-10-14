021a778378059d43e6203df026417b5de397c5bb
antimo2001 <antimo2001@gmail.com>
2018-10-13 23:59:02 -0400

    fix config for conditional video-storage

--- server.config.js
+++ server.config.js
@@ -1,19 +1,26 @@
 const path = require('path');
+const debug = require('debug')('prac-video-server:server.config');
 const DEFAULT_VIDEO_PATH = path.join(__dirname, 'testdata');
+const { PORT, HOMEPATH, VIDEO_STORAGE_DIR } = process.env;
+const IS_BRANCH = (/branch/).test(HOMEPATH);
 
-/**
- * Return the path to the directory of files
- */
-const setStoragePath = () => {
-  const { HOMEPATH, VIDEO_STORAGE_DIR } = process.env;
-  //If HOMEPATH contains branch, then use VIDEO_STORAGE_DIR, else use default
-  return (/branch/).test(HOMEPATH) ? VIDEO_STORAGE_DIR : DEFAULT_VIDEO_PATH;
-}
-
-module.exports = {
-  port: process.env.PORT || 4242,
-  sortFolders: 'desc',
-  storageDir: setStoragePath(),
+const defaults = {
+  port: 4242,
+  sortFolders: 'asc',
+  storageDir: DEFAULT_VIDEO_PATH,
   rxFileFilter: (/\.(mp4|MTS|mpg)/i),
   rxFolders: (/\b[2-9]|(B[LR]\d*)\b/i),
-}
+};
+
+const branchConf = {
+  port: 6996,
+  sortFolders: 'desc',
+  storageDir: VIDEO_STORAGE_DIR
+};
+
+let config = IS_BRANCH ? branchConf : defaults;
+config = Object.assign(defaults, config);
+
+debug('server.config==%j', config);
+
+module.exports = config;
