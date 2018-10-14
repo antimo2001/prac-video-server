9eaffa4514589d6e828f3b52647c47e99bc2b0b1
antimo2001 <antimo2001@gmail.com>
2018-10-13 23:37:29 -0400

    conditional directory

--- server.config.js
+++ server.config.js
@@ -1,11 +1,19 @@
 const path = require('path');
-const testdataDir = path.join(__dirname, 'testdata');
+const DEFAULT_VIDEO_PATH = path.join(__dirname, 'testdata');
+
+/**
+ * Return the path to the directory of files
+ */
+const setStoragePath = () => {
+  const { HOMEPATH, VIDEO_STORAGE_DIR } = process.env;
+  //If HOMEPATH contains branch, then use VIDEO_STORAGE_DIR, else use default
+  return (/branch/).test(HOMEPATH) ? VIDEO_STORAGE_DIR : DEFAULT_VIDEO_PATH;
+}
 
 module.exports = {
   port: process.env.PORT || 4242,
   sortFolders: 'desc',
-  // storageDir: process.env.VIDEO_STORAGE_DIR || testdataDir,
-  storageDir: testdataDir,
+  storageDir: setStoragePath(),
   rxFileFilter: (/\.(mp4|MTS|mpg)/i),
   rxFolders: (/\b[2-9]|(B[LR]\d*)\b/i),
 }
